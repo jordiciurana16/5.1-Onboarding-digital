bbe026cf15d0a9a8a312ceeb4949e8a532944d10
jordiciurana16 <jciuranas@gmail.com>
2025-03-24 13:35:50 +0100

    Import images configuration

--- src/app/steps.service.ts
+++ src/app/steps.service.ts
@@ -10,21 +10,21 @@ export class StepsService {
       title: 'Dedica moltes hores',
       description:
         'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
-      image: 'https://via.placeholder.com/150',
+      image: 'assets/meditation.svg',
       bgcolor: '#FF5733',
     },
     {
       title: 'Programa projectes propis',
       description:
         'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
-      image: 'https://via.placeholder.com/150',
+      image: 'assets/programming.svg',
       bgcolor: '#FF5733',
     },
     {
       title: 'Procura descansar',
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      image: 'https://via.placeholder.com/150',
+      image: '/assets/time_managment.svg',
       bgcolor: '#FF5733',
     },
   ];
