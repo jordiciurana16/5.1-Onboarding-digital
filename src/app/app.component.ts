3c1e9287d607477b76d376ea11c0194162b00964
jordiciurana16 <jciuranas@gmail.com>
2025-03-24 11:16:28 +0100

    Commit Environment set up

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HomeComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'app';
+}
