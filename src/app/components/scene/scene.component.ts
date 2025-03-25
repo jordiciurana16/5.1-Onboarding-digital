e31941004d7ca1472d1e5f5877afd6c6067d7866
jordiciurana16 <jciuranas@gmail.com>
2025-03-25 12:09:24 +0100

    getter button visibility

--- src/app/components/scene/scene.component.ts
+++ src/app/components/scene/scene.component.ts
@@ -4,7 +4,7 @@ import {
   bootstrapArrowLeft,
   bootstrapArrowRight,
 } from '@ng-icons/bootstrap-icons';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { ISteps } from '../../i-steps';
 
 @Component({
@@ -16,4 +16,28 @@ import { ISteps } from '../../i-steps';
 })
 export class SceneComponent {
   @Input() steps: ISteps[] = [];
+
+  currentStep = 1;
+
+  get previousButtonVisibility(): string {
+    return this.currentStep === 2 || this.currentStep === 3
+      ? 'visible'
+      : 'hidden';
+  }
+
+  get nextButtonVisibility(): string {
+    return this.currentStep === 1 || this.currentStep === 2
+      ? 'visible'
+      : 'hidden';
+  }
+
+  previousUser() {
+    this.currentStep--;
+    console.log(this.currentStep);
+  }
+
+  nextUser() {
+    this.currentStep++;
+    console.log(this.currentStep);
+  }
 }
