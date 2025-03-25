6b27715ea951aadd361b59620dc622d4f13a4622
jordiciurana16 <jciuranas@gmail.com>
2025-03-25 10:59:13 +0100

    button style

--- src/app/components/scene/scene.component.ts
+++ src/app/components/scene/scene.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import {
+  bootstrapArrowLeft,
+  bootstrapArrowRight,
+} from '@ng-icons/bootstrap-icons';
 import { CommonModule } from '@angular/common';
 import { ISteps } from '../../i-steps';
 
 @Component({
   selector: 'app-scene',
-  imports: [CommonModule],
+  imports: [CommonModule, NgIcon],
   templateUrl: './scene.component.html',
   styleUrl: './scene.component.scss',
+  viewProviders: [provideIcons({ bootstrapArrowLeft, bootstrapArrowRight })],
 })
 export class SceneComponent {
   @Input() steps: ISteps[] = [];
